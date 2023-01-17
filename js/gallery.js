40f2ec19fbfea93d38375f91782e8358ea8c10f8
danishubin <dshubin@eastsideprep.org>
2023-01-16 19:16:49 -0800

    Before After Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,22 @@
-var slider = document.getElementById("divide");
-var before = document.getElementsByClassName("before");
-var sliderbutton = document.getElementById("slider-button");
+var range1 = document.getElementById("range1");
+var before1 = document.getElementById("before1");
+var slider1 = document.getElementById("slider1");
+var sliderbutton1 = document.getElementById("slider-button1");
+
+var range2 = document.getElementById("range2");
+var before2 = document.getElementById("before2");
+var slider2 = document.getElementById("slider2");
+var sliderbutton2 = document.getElementById("slider-button2");
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    for (var i = 0; i < before.length; i++) {
-        before[i].style.width = this.value + "%";
-        sliderbutton.style.left = "calc(" + this.value + "% - 18px)";
-    }
+range1.oninput = function() {
+    before1.style.width = this.value + "%";
+    slider1.style.left = "calc(" + this.value + "%)";
+    sliderbutton1.style.left = "calc(" + this.value + "%)";
+}
+
+range2.oninput = function() {
+    before2.style.width = this.value + "%";
+    slider2.style.left = "calc(" + this.value + "%)";
+    sliderbutton2.style.left = "calc(" + this.value + "%)";
 }
