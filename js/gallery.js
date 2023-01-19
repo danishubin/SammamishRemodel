939d9901a75f3ec10b9563e665de657c9af6b89b
danishubin <dshubin@eastsideprep.org>
2023-01-18 16:33:43 -0800

    Fixed Image Links

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 var current = 0;
-var afterImages = ["images/After\ 1.PNG", "images/After\ 2.PNG", "images/After\ 3.PNG"]
-var beforeImages = ["images/Before\ 1.PNG", "images/Before\ 2.PNG", "images/Before\ 3.PNG"]
+var afterImages = ["images/After\ 1.png", "images/After\ 2.png", "images/After\ 3.png"]
+var beforeImages = ["images/Before\ 1.png", "images/Before\ 2.png", "images/Before\ 3.png"]
 
 var gallery = document.getElementById("gallery");
 var left = document.getElementById("left");
